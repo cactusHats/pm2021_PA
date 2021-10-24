129c4e593076bf63f85014e3f58a6ed349341668
Masashi-Seki <46625106+cactusHats@users.noreply.github.com>
2021-10-24 18:42:34 +0900

    sleepの単位を修正

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -223,7 +223,7 @@ void ofApp::reply() {
 	ofxOscMessage message;
 	message.setAddress("/pmap/connection/responce/PA"); //change URL according to device name.
 
-	sleep(1000);
+	sleep(1); //sec
 	sender.sendMessage(message);
 
 	cout << "replay" << endl;
